20a175419d4c53e940d6c2b4f4f7e40dd7afb1aa
Charbel Kisso <charbel@s5consulting.se>
2024-09-04 12:40:45 +0200

    update JS interface

--- www/ZebraPluginBtPrint.js
+++ www/ZebraPluginBtPrint.js
@@ -1,26 +1,9 @@
-cordova.define("s5-zebra-plugin-btprint.ZebraPluginBtPrint", function(require, exports, module) {
-    let exec = require('cordova/exec');
-    
-    let App = function() {
-        this.initialize = function (delay, wildcard, printerName, cancelButtonName) {
-            exec(null, null, "ZebraPluginBtPrint", "initialize", [
-              delay || 0,
-              wildcard,
-              printerName,
-              cancelButtonName,
-            ]);
-          };
-        
-        this.print = function (mac, data, statusCallback) {
-            exec(statusCallback, statusCallback, 'ZebraPluginBtPrint', 'print', [mac, data]);
-        };
-    };
-    
-    module.exports = App;
-    
-    
-    
-    
-    
-    });
-    
+var exec = require('cordova/exec');
+
+exports.initialize = function (delay, wildcard, printerName, cancelButtonName) {
+    exec(null, null, 'ZebraPluginBtPrint', 'initialize', [delay || 0, wildcard, printerName, cancelButtonName]);
+};  
+
+exports.print = function (mac, data, statusCallback) {  
+    exec(statusCallback, statusCallback, 'ZebraPluginBtPrint', 'print', [mac, data]);
+};
